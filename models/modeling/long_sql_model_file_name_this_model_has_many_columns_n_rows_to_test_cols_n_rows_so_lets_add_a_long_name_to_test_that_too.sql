23e8b933f92f7c52f7704d47dcf46eed2f8f2445
Tim Klense <tim.klense@fishtownanalytics.com>
2024-05-08 19:32:24 +0000

    update

--- models/modeling/long_sql_model_file_name_this_model_has_many_columns_n_rows_to_test_cols_n_rows_so_lets_add_a_long_name_to_test_that_too.sql
+++ models/modeling/long_sql_model_file_name_this_model_has_many_columns_n_rows_to_test_cols_n_rows_so_lets_add_a_long_name_to_test_that_too.sql
@@ -2,7 +2,7 @@
 {% for j in range(80) %}
   select
   {% for i in range(180) %}
-    '{{ chars[((i + j) % 52):((i + j) % 52) + 5 + ((i + j) % 28)] }}' as col_{{ i + (j*2) }}
+    '{{ chars[((i + j) % 22):((i + j) % 22) + 5 + ((i + j) % 21)] }}' as column_name_is_really_long_bcuz_some_people_have_long_col_names_so_we_might_want_to_test_that_too_{{ i + (j*2) }}
     {%- if not loop.last %}, {% endif -%}
   {% endfor %}
   {%- if not loop.last %} UNION ALL {% endif %}
