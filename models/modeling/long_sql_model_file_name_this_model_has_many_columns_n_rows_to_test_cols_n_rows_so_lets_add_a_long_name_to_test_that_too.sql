7a9be85fbcc03f6e71f83e06f112be226e85724e
Tim Klense <tim.klense@fishtownanalytics.com>
2024-05-13 18:43:13 +0000

    update

--- models/modeling/long_sql_model_file_name_this_model_has_many_columns_n_rows_to_test_cols_n_rows_so_lets_add_a_long_name_to_test_that_too.sql
+++ models/modeling/long_sql_model_file_name_this_model_has_many_columns_n_rows_to_test_cols_n_rows_so_lets_add_a_long_name_to_test_that_too.sql
@@ -1,8 +1,8 @@
-{% set chars = "abcdefghijklmnopqrstuv" %}
-{% for j in range(80) %}
+{% set chars = "abcdefghij" %}
+{% for j in range(70) %}
   select
-  {% for i in range(180) %}
-    '{{ chars[((i + j) % 22):((i + j) % 22) + 5 + ((i + j) % 21)] }}' as column_name_is_really_long_bcuz_some_people_have_long_col_names_so_we_might_want_to_test_that_too_{{ i + (j*2) }}
+  {% for i in range(165) %}
+    '{{ chars[((i + j) % 7):((i + j) % 722) + 5 + ((i + j) % 6)] }}' as column_name_is_really_long_bcuz_some_people_have_long_col_names_so_we_might_want_to_test_that_too_{{ i + (j*2) }}
     {%- if not loop.last %}, {% endif -%}
   {% endfor %}
   {%- if not loop.last %} UNION ALL {% endif %}
