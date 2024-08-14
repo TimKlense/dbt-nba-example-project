7afd58fc35102cdaa73c28fc728fab5f7d681afe
Tim Klense <tim.klense@fishtownanalytics.com>
2024-08-14 01:31:00 +0000

    a

---
+++ models/staging/stg_long_name.sql
@@ -0,0 +1,19 @@
+-- models/stg_game_info.sql
+
+-- Define the staging model named "stg_game_info"
+
+with source as (
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+    select * from {{ ref('raw_long_name') }}
+),
+renamed as (
+    select
+        COLUMN_NAME_LONG_TO_SHOW_THAT_IT_GOES_OFF_THE_SCREEN_FOR_VIS_EDITOR0,
+        COLUMN_NAME_LONG_TO_SHOW_THAT_IT_GOES_OFF_THE_SCREEN_FOR_VIS_EDITOR1,
+    from source
+)
+
+select * from renamed
